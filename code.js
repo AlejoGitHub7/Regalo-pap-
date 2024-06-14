2b5bed41e0aeb30a15715e43e9915231316822e3
Alejandro Muñoz <alemunvan@gmail.com>
2024-06-13 20:07:43 -0500

    Entrar a segunda seccion

---
+++ code.js
@@ -0,0 +1,8 @@
+const cabecera = document.getElementById("cabecera")
+const TituloIngresar = document.getElementById("Titulo2")
+
+function Comienzo(){
+    cabecera.classList.add("inactive")
+    TituloIngresar.classList.toggle("inactive")
+
+}
