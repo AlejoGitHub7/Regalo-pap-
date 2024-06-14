81fe648204f8687b0c5fa05715682389b2c205ef
Alejandro Muñoz <alemunvan@gmail.com>
2024-06-14 06:43:38 -0500

    Cambio de pestaña y mejora de los inputs y el estilo

--- code.js
+++ code.js
@@ -1,8 +1,12 @@
 const cabecera = document.getElementById("cabecera")
 const TituloIngresar = document.getElementById("Titulo2")
+const Inputs = document.getElementById("Inputs")
+const Obtener = document.getElementById("Obtener")
 
 function Comienzo(){
     cabecera.classList.add("inactive")
     TituloIngresar.classList.toggle("inactive")
+    Inputs.classList.toggle("inactive")
+    Obtener.classList.toggle("inactive")
 
 }
