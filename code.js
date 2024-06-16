cbdd9b523ce14c00a96683a804e971c6b7f5514c
Alejandro Muñoz <alemunvan@gmail.com>
2024-06-16 08:08:16 -0500

    Fin del programa

--- code.js
+++ code.js
@@ -2,6 +2,16 @@ const cabecera = document.getElementById("cabecera")
 const TituloIngresar = document.getElementById("Titulo2")
 const Inputs = document.getElementById("Inputs")
 const Obtener = document.getElementById("Obtener")
+const error = document.getElementById("error")
+const d1 = document.getElementById("Digito1")
+const d2 = document.getElementById("Digito2")
+const d3 = document.getElementById("Digito3")
+const d4 = document.getElementById("Digito4")
+const premio = document.getElementById("premio")
+let d1value
+let d2value
+let d3value
+let d4value
 
 function Comienzo(){
     cabecera.classList.add("inactive")
@@ -9,4 +19,24 @@ function Comienzo(){
     Inputs.classList.toggle("inactive")
     Obtener.classList.toggle("inactive")
 
+}
+
+function comparar(){
+    if(d1value == 8 && d2value == 5 && d3value == 2 && d4value == 1){
+        premio.classList.toggle("inactive")
+        TituloIngresar.classList.add("inactive")
+        Inputs.classList.add("inactive")
+        Obtener.classList.add("inactive")
+
+    }else{
+        error.classList.toggle("inactive")
+    }
+}
+
+function obtenerValores(){
+    d1value = d1.value
+    d2value = d2.value
+    d3value = d3.value
+    d4value = d4.value
+    comparar()
 }
